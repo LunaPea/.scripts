626d1569372f35fe99db138fc0c02c1c174e7217
WeraPea <sokneip@tuta.io>
2023-02-24 17:57:15 +0100

    added virtual mic sink creation

--- audio.sh
+++ audio.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if ! pactl list modules | grep Virtual-Mic-Sink; then
+  pactl load-module module-null-sink \
+        sink_name=audiorelay-virtual-mic-sink \
+        sink_properties=device.description=Virtual-Mic-Sink
+fi
 export DISPLAYY=:9
 Xvfb $DISPLAYY &
 x11vnc -display $DISPLAYY -localhost -bg
